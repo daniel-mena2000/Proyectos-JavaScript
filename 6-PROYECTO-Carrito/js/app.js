46bfa0ac076b668eb6ec4ef58863de8b23f538bb
Daniel Mena <dmena044@gmail.com>
2025-01-27 11:39:28 -0600

    Proyectos JS 1

---
+++ 6-PROYECTO-Carrito/js/app.js
@@ -0,0 +1,5 @@
+const carrito = document.querySelector('#carrito');
+// Para buscar dentro de este elemento a tbody
+const contenedorCarrito = document.querySelector('#lista-carrito tbody');
+const vaciarCarritoBtn = document.querySelector("#vaciar-carrito");
+const listaCursos = document.querySelector('#Lista-cursos');
